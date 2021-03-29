c6eaf94a10a711a22a8f379bd2600cdb4047f184
soomskim <61723783+soomskim@users.noreply.github.com>
2021-03-29 02:57:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -16,71 +16,30 @@ for (i = 0; i < acc.length; i++) {
 
 
 //IMG GALLERY - slideshow ------------- https://www.w3schools.com/howto/howto_js_slideshow.asp
-var slideIndex = 1;
-showSlides(slideIndex);
+//------------- multiple sliders on 1pg- https://stackoverflow.com/questions/54085770/how-to-get-multiple-slider-on-one-page
+var slideIndex = [1, 1];
+var slideId = ["gallery-slide1", "gallery-slide2"]
+showSlides (1, 0);
+showSlides (1, 1);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+//showSlides(slideIndex);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
 }
 
-function showSlides(n) {
+function showSlides(n, no) {
   var i;
-  var slides = document.getElementsByClassName("gallery-slide");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  var slides = document.getElementsByClassName(slideId[no]);
+  if (n > slides.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
 
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex[no]-1].style.display = "block";  
 }
-/*const gallerySlide = document.querySelector('.gallery-slide');
-const galleryImgs = document.querySelectorAll('.gallery-slide img');
-
-//buttons
-const prevBtn = document.querySelector('.prevBtn');
-const nxtBtn = document.querySelector('.nxtBtn');
-
-//counter
-let counter = 1;
-const size = galleryImgs[0].clientWidth;
-
-gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-//button listeners
-nxtBtn.addEventListener('click',() => {
-	if (counter >= galleryImgs.length - 1) return;
-
-	gallerySlide.style.transition = "none";
-	counter++;
-
-	gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-prevBtn.addEventListener('click',() => {
-	if (counter <= 0) return;
-
-	gallerySlide.style.transition = "none";
-	counter--;
-
-	gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-//loop back to img
-gallerySlide.addEventListener('transitionend', () => {
-	if (galleryImgs[counter].id === 'lastImg') {
-		gallerySlide.style.transition = "none";
-		counter = galleryImgs.length - 2;
-		gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-	}
-
-	if (galleryImgs[counter].id === 'firstImg') {
-		gallerySlide.style.transition = "none";
-		counter = galleryImgs.length - counter;
-		gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-	}
-});*/
