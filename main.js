e1992091ebcdc2e782df72426d1bf413dcea8a7d
soomskim <61723783+soomskim@users.noreply.github.com>
2021-04-02 02:27:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -17,10 +17,11 @@ for (i = 0; i < acc.length; i++) {
 
 //IMG GALLERY - slideshow ------------- https://www.w3schools.com/howto/howto_js_slideshow.asp
 //------------- multiple sliders on 1pg- https://stackoverflow.com/questions/54085770/how-to-get-multiple-slider-on-one-page
-var slideIndex = [1, 1];
-var slideId = ["gallery-slide1", "gallery-slide2"]
-showSlides (1, 0);
-showSlides (1, 1);
+var slideIndex = [1, 1, 1];
+var slideId = ["gallery-slide0", "gallery-slide1", "gallery-slide2"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
 
 //showSlides(slideIndex);
 
